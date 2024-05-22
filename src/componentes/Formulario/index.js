18db0f57bbff94aef89058ea8c1f42e8d9b09eca
Fabio Faria <fhabyomontilla@gmail.com>
2024-05-22 10:02:32 -0300

    Criado evento do form e passado propriedade required aos componentes

--- src/componentes/Formulario/index.js
+++ src/componentes/Formulario/index.js
@@ -12,14 +12,19 @@ const Formulario = () => {
         'UX / Design'
     ]
 
+    const aoSalvar = (evento) => {
+        evento.preventDefault()
+        alert('Form Submetido')
+    }
+
     return (
         <section className='formulario'>
-            <form>
+            <form onSubmit={aoSalvar}>
                 <h2>Preencha os dados para criar o card do colaborador</h2>
-                <CampoTexto label="Nome" placeholder="Digite seu nome" />
-                <CampoTexto label="Cargo" placeholder="Digite seu cargo" />
+                <CampoTexto obrigatorio={true} label="Nome" placeholder="Digite seu nome" />
+                <CampoTexto obrigatorio={true} label="Cargo" placeholder="Digite seu cargo" />
                 <CampoTexto label="Imagem" placeholder="Digite o endereço da sua imagem" />
-                <ListaSuspensa itens={times} label="Time"/>
+                <ListaSuspensa obrigatorio={true} itens={times} label="Time"/>
                 <Botao>Criar Card</Botao>
             </form>
         </section>
