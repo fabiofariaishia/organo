e1320359e9a58b5ef563c85cf6ee5911ab81fbcd
Fabio Faria <fhabyomontilla@gmail.com>
2024-05-22 15:01:43 -0300

    Corrigido reset lista suspensa

--- src/componentes/ListaSuspensa/index.js
+++ src/componentes/ListaSuspensa/index.js
@@ -7,6 +7,7 @@ const ListaSuspensa = (props) => {
         <div className='lista-suspensa'>
             <label>{props.label}</label>
             <select required={props.obrigatorio} value={props.valor} onChange={evento => props.aoAlterar(evento.target.value)}>
+                <option value=""></option>
                 {props.itens.map(item => <option key={item}>{item}</option>)}
             </select>
         </div>
